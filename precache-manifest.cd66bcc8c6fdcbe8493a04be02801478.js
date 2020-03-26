8662c12db23d1c305e605557294007af70cbf405
kobylan <kobylan.kaz@gmail.com>
2020-03-26 06:19:48 +0600

    Updates

---
+++ precache-manifest.cd66bcc8c6fdcbe8493a04be02801478.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6cffcc74553d6aa52ff86c22ec3b2f1",
+    "url": "/nurisgeo2/index.html"
+  },
+  {
+    "revision": "3dba7f46f57206a8c3f7",
+    "url": "/nurisgeo2/static/css/2.81fc3d16.chunk.css"
+  },
+  {
+    "revision": "a5847c4d918c944edc00",
+    "url": "/nurisgeo2/static/css/main.828c10eb.chunk.css"
+  },
+  {
+    "revision": "3dba7f46f57206a8c3f7",
+    "url": "/nurisgeo2/static/js/2.26d506dd.chunk.js"
+  },
+  {
+    "revision": "5d4bd2d2451ea0aa97e0623a6ad91d29",
+    "url": "/nurisgeo2/static/js/2.26d506dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5847c4d918c944edc00",
+    "url": "/nurisgeo2/static/js/main.c0e3a9c7.chunk.js"
+  },
+  {
+    "revision": "4b773cff7d9d90e3fdde",
+    "url": "/nurisgeo2/static/js/runtime-main.b3f45fe5.js"
+  },
+  {
+    "revision": "1e0a1a213ce0adc541198f07444d6da6",
+    "url": "/nurisgeo2/static/media/change.1e0a1a21.gif"
+  },
+  {
+    "revision": "3d2fa2e544004aef2ea641698b715af6",
+    "url": "/nurisgeo2/static/media/feather.3d2fa2e5.woff"
+  },
+  {
+    "revision": "6a750f0119ba9c8ab8a994a4796599a6",
+    "url": "/nurisgeo2/static/media/feather.6a750f01.ttf"
+  },
+  {
+    "revision": "931c9e74d0b20947054bfe6a5b74a838",
+    "url": "/nurisgeo2/static/media/feather.931c9e74.eot"
+  },
+  {
+    "revision": "b23df4db71a29dbb733a0e555a7db8f9",
+    "url": "/nurisgeo2/static/media/feather.b23df4db.svg"
+  },
+  {
+    "revision": "eb4e894d949eb0ce8f21368b70f3dd3b",
+    "url": "/nurisgeo2/static/media/vuesax-login-bg.eb4e894d.jpg"
+  }
+]);
